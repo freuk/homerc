f45326f2196152b837a30db34009d5750174a9bd
fre <fre@freux.fr>
2020-09-22 15:58:03 -0500

    tig

--- home.nix
+++ home.nix
@@ -1,21 +1,20 @@
-{ config, pkgs, ... }:
-{
-  # Let Home Manager install and manage itself.
+{ config, pkgs, ... }: {
   programs.home-manager.enable = true;
+  home.stateVersion = "20.03";
 
-  #xdg.enable =true;
   xdg.configFile."kak/kakrc".source = ./kak/kakrc;
   xdg.configFile."kak/autoload".source = ./kak/autoload;
   xdg.configFile."kak/colors".source = ./kak/colors;
-  home.packages = [pkgs.kakoune];
 
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
-  home.stateVersion = "20.03";
+  home = {
+
+    packages = [
+      pkgs.kakoune
+      pkgs.nnn
+      (pkgs.writeShellScriptBin "t" "${pkgs.tig}/bin/tig status $@").out
+    ];
+
+    sessionVariables = { NNN_OPENER = nnn/nuke; };
+  };
+  home.file."tig/tig.cfg".source = ./tig.cfg;
 }
